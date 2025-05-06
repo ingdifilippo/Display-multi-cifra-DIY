ae7cffe639e9e0b8644bd035d638bd1eaa8b233b
Tyler <davidedifilippo@iisdaostalaquila.it>
2025-05-06 19:09:48 +0200

    Create conteggio_4_digit.ino

---
+++ conteggio_4_digit.ino
@@ -0,0 +1,95 @@
+#include <Arduino.h>
+
+const int A = 5;
+const int B = 4;
+const int C = 3;
+const int D = 2;
+
+const int D4 = 6;
+const int D3 = 7;
+const int D2 = 8;
+const int D1 = 9;
+
+void displayDigit(int d, int n) {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(9-i, (i == d? LOW : HIGH));     
+  }
+  digitalWrite(5, (n & B00000001));
+  digitalWrite(4, (n & B00000010));
+  digitalWrite(3, (n & B00000100));
+  digitalWrite(2, (n & B00001000));    
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  for (int i = 2; i <= 9; i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);  
+  }
+  
+ 
+
+  digitalWrite(D1, LOW); 
+  digitalWrite(D2, HIGH); 
+  digitalWrite(D3, HIGH); 
+  digitalWrite(D4, HIGH);
+  delay(1000);
+    
+  digitalWrite(D, LOW); 
+  digitalWrite(C, LOW); 
+  digitalWrite(B, LOW); 
+  digitalWrite(A, LOW);
+  delay(1000);
+    
+  digitalWrite(D, LOW); 
+  digitalWrite(C, LOW); 
+  digitalWrite(B, LOW); 
+  digitalWrite(A, HIGH);
+  delay(1000);
+    
+  digitalWrite(D, LOW); 
+  digitalWrite(C, LOW); 
+  digitalWrite(B, HIGH); 
+  digitalWrite(A, LOW);
+  delay(1000);
+    
+  digitalWrite(D, LOW); 
+  digitalWrite(C, LOW); 
+  digitalWrite(B, HIGH); 
+  digitalWrite(A, HIGH);
+  delay(1000);
+    
+  digitalWrite(D, LOW); 
+  digitalWrite(C, HIGH); 
+  digitalWrite(B, LOW); 
+  digitalWrite(A, LOW);
+  delay(1000);
+}
+
+int c = 0;
+unsigned long dt, t1;
+
+
+void loop() {
+  displayDigit(0, (c%10));
+  delay(4);
+  displayDigit(1, ((c/10)%10));
+  delay(4);
+  displayDigit(2, ((c/100)%10));
+  delay(4);
+  displayDigit(3, ((c/1000)%10));
+  delay(4);
+  
+  dt = millis() - t1;
+  if (dt >= 1000) 
+   {
+    c++;
+    t1 = millis();
+   }  
+  /* digitalWrite(D1, LOW); 
+  delay(1000);
+  digitalWrite(D1, HIGH); 
+  delay(1000); */
+
+}
